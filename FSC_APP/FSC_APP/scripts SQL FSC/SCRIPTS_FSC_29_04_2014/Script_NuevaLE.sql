bbe35aadb611cf25d3ad4e5a21e130919d6ad615
neogermax <neogermax@gmail.com>
2014-04-29 10:52:18 -0500

    script de actualizacion lineas estrategicas

---
+++ FSC_APP/FSC_APP/scripts SQL FSC/SCRIPTS_FSC_29_04_2014/Script_NuevaLE.sql
@@ -0,0 +1,10 @@
+use FSC_eProject
+-- script insercion de una nuva linea estrategica
+insert into StrategicLine (Code, Name, IdStrategicObjective, IdManagment, Enabled,IdUser, CreateDate)
+values ('1.Vida Saludable','1.Vida Saludable','36','4','1','149','2014-04-29 09:15:00.000')
+insert into StrategicLine (Code, Name, IdStrategicObjective, IdManagment, Enabled,IdUser, CreateDate)
+values ('2. Acceso al Conocimiento','2. Acceso al Conocimiento','36','4','1','149','2014-04-29 09:15:00.000')
+insert into StrategicLine (Code, Name, IdStrategicObjective, IdManagment, Enabled,IdUser, CreateDate)
+values ('3. Vida Digna','3. Vida Digna','36','4','1','149','2014-04-29 09:15:00.000')
+insert into StrategicLine (Code, Name, IdStrategicObjective, IdManagment, Enabled,IdUser, CreateDate)
+values ('4. Procesos Estratégicos y de Soporte ','4. Procesos Estratégicos y de Soporte ','36','4','1','149','2014-04-29 09:15:00.000')
