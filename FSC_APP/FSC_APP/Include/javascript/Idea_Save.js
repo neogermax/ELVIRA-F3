f07a38d1bc456878f14c83c9b648da844af18292
neogermax <neogermax@gmail.com>
2014-06-13 15:03:45 -0500

    compilacion de f_3

--- FSC_APP/FSC_APP/Include/javascript/Idea_Save.js
+++ FSC_APP/FSC_APP/Include/javascript/Idea_Save.js
@@ -24,39 +24,5 @@ function ChargeOfArrangements() {
         listubicaciones[0] = "vacio_ojo";
     }
 
-    //recorer array para el ingreso de actores
-    for (item in arrayActor) {
-        listactores.push(JSON.stringify(arrayActor[item]));
-    }
-
-    //recorer array para el ingreso de flujos
-    for (item in arrayflujosdepago) {
-        listflujos.push(JSON.stringify(arrayflujosdepago[item]));
-        console.log(arrayflujosdepago[item]);
-
-    }
-    //validar si el array tiene datos   
-    if (listflujos.length == 0) {
-        listflujos[0] = "vacio_ojo";
-    }
-
-    for (item in matriz_flujos) {
-        listdetallesflujos.push(JSON.stringify(matriz_flujos[item]));
-    }
-
-    //validar si el array tiene datos
-    if (listdetallesflujos.length == 0) {
-        listdetallesflujos[0] = "vacio_ojo";
-    }
-
-    //recorrer el array para el ingreso archivos 
-    for (item in arrayFiles) {
-        listfiles.push(JSON.stringify(arrayFiles[item]));
-    }
-
-    //validar si tiene datos
-    if (listfiles.length == 0) {
-        listfiles[0] = "vacio_ojo";
-    }
-
+   
 }
